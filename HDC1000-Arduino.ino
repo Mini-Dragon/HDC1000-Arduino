3284bf9d403c1f0a61094c743fdf4f6408d573fa
Mini Dragon <projecteurs@gmail.com>
2015-12-07 19:04:08 -0800

    Add feedback FSK function

--- HDC1000-Arduino.ino
+++ HDC1000-Arduino.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>
-#include <HDC1000.h>
+#include "HDC1000.h"
+#define feedback 7
  
 HDC1000 mySensor;
 //HDC1000 mySensor(0x40, 2) <-- DRDYn enabled and connected to Arduino pin 2 (allows for faster measurements).
@@ -7,13 +8,43 @@ HDC1000 mySensor;
 void setup(){
 	Serial.begin(9600);
 	mySensor.begin();
+	pinMode(feedback, OUTPUT);
 }
- 
+
+void senddata(int data, int howmanybits){
+	for(int i=howmanybits+1; i>=0; i--){
+		int b = ( data & (1 << i) ) >> i;
+		if (b==1){
+			for (int j=3; j>=0; j--){
+				digitalWrite(feedback, 0);
+				delay(1);
+				digitalWrite(feedback, 1);
+				delay(1);
+			}
+		}
+		else{
+			digitalWrite(feedback, 0);
+			delay(1);
+			digitalWrite(feedback, 1);
+			delay(2);
+			digitalWrite(feedback, 0);
+			delay(2);
+			digitalWrite(feedback, 1);
+			delay(2);
+			digitalWrite(feedback, 0);
+			delay(1);
+		}
+	}
+}
+
 void loop(){
-	Serial.print("Temperature: ");
-	Serial.print(mySensor.getTemp()); 
-	Serial.print("C, Humidity: ");     
-	Serial.print(mySensor.getHumi());
-	Serial.println("%");
-	delay(1000);
+	int t = mySensor.getRawTemp(); 
+	int h = mySensor.getRawHumi();
+        Serial.print(t);
+        Serial.print("'");
+        Serial.println(h);
+        senddata(t, 16);
+        senddata(h, 16);
+        digitalWrite(feedback, 0);
+	delay(5);
 }
